7f5aadad445b31f890526790c25b302e1205f78d
sujoy-dam <sujooy01@gmail.com>
2024-10-13 19:00:57 +0600

    show details modal

--- js/phone.js
+++ js/phone.js
@@ -6,11 +6,14 @@ const loadAllPhone = async () => {
 }
 const displayAllPhone = (phones) => {
     // console.log(phones)
+    // if(phones.length > 5){
+    //     phones.slice(0,5)
+    // }
     document.getElementById('spinner').classList.add('hidden')
 
     const phonesContainer = document.getElementById('phones-container')
-    if(phones.length ===0){
-        phonesContainer.innerHTML=`
+    if (phones.length === 0) {
+        phonesContainer.innerHTML = `
             <h1 class="text-8xl font-extrabold text-violet-600 text-center">NO phones is available</h1>
         `
         phonesContainer.classList.remove('grid')
@@ -35,7 +38,7 @@ const displayAllPhone = (phones) => {
                     <h2 class="card-title font-bold text-2xl">${phone_name}</h2>
                     <p>If a dog chews shoes whose shoes does he choose?</p>
                     <div class="card-actions">
-                        <button class="btn btn-primary" id="details">Show Details</button>
+                        <button class="btn btn-primary" onclick="loadDetails('${slug}')">Show Details</button>
                     </div>
                 </div>
             </div>
@@ -57,18 +60,54 @@ const loadSearchPhones = async (phoneName) => {
     const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${phoneName}`)
     const data = await res.json()
     console.log(data.data)
-    setTimeout(()=>{
+    setTimeout(() => {
 
         displayAllPhone(data.data)
     }, 2000)
 }
 
-const displaySearchPhones =()=>{
-
-}
-// const loadDetails = ()=>{
-
+// const displaySearchPhones =()=>{
+//     console.log('connected')
 // }
+const loadDetails = async (id) => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`)
+    const data = await res.json()
+    displayModal(data.data)
+    my_modal_5.showModal()
+}
+const displayModal = (details) => {
+    console.log(details)
+    const { brand, image, name, mainFeatures, slug, others, releaseData } = details
+    const modalBox = document.getElementById('modal')
+
+    modalBox.innerHTML = `
+        <dialog id="my_modal_5" class="modal modal-bottom sm:modal-middle">
+            <div class="modal-box">
+                <div>
+                    <img class="mx-auto" src=${image}
+                    <h3 class="text-2xl font-bold">${name}</h3>
+                    <p><span class="font-lg font-bold">Storage:</span> ${mainFeatures.storage}</p>
+                    <p><span class="font-lg font-bold">Display Size:</span> ${mainFeatures.displaySize}</p>
+                    <p><span class="font-lg font-bold">Chipset:</span> ${mainFeatures.chipSet}</p>
+                    <p><span class="font-lg font-bold">Memory:</span> ${mainFeatures.memory}</p>
+                    <p><span class="font-lg font-bold">Slug:</span> ${slug}</p>
+                    <p><span class="font-lg font-bold">Release Data:</span> ${releaseData?releaseData:'Not available'}</p>
+                    <p><span class="font-lg font-bold">Brand:</span> ${brand}</p>
+                    <p><span class="font-lg font-bold">GPS:</span> ${others?.GPS? others.GPS : "Not available"}</p>
+                    <p><span class="py-4">Press ESC key or click the button below to close</p>
+                </div>
+                <div class="modal-action">
+                    <form method="dialog">
+                        <!-- if there is a button in form, it will close the modal -->
+                        <button class="btn">Close</button>
+                    </form>
+                </div>
+            </div>
+        </dialog>
+    
+    `
+    // modalBox.append(div)
+}
 
 
 // const searchLoadPhones = ()=>{
